006a10288c1753d90f814187d5b176d234e71b37
Alejandro Larreta Valenzuela <larreta_vzla@hotmail.com>
2024-10-02 13:17:32 -0700

    Cambio a mysql2

--- mysqlconfig.js
+++ mysqlconfig.js
@@ -1,13 +1,13 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER, DB_PORT } = require('./config.js');
 //Conexion a la base de datos MySQL
 
 const db = mysql.createConnection({
+    host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORD,
-    host: DB_HOST,
+    database: DB_NAME,
     port: DB_PORT,
-    database: DB_NAME
 });
 
 // Conectar a la base de datos
